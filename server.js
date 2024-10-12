f8f406039c44943ca73089372618ca522a6dde66
Bailey Latimer <baileyl@beautyindustrygroup.com>
2024-10-11 22:32:09 -0700

    Revert to working Netlify configuration with Sanity integration

--- server.js
+++
@@ -1,8 +0,0 @@
-import { createRequestHandler } from "@netlify/remix-adapter";
-import * as build from "@remix-run/dev/server-build";
-
-export const handler = createRequestHandler({
-  build,
-  // eslint-disable-next-line no-undef
-  mode: process.env.NODE_ENV || "production",
-});
