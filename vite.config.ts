3a980006ed49fa731a9ce86990fbc658dfc54212
Bailey Latimer <baileyl@beautyindustrygroup.com>
2024-10-11 23:16:41 -0700

    Update vite.config.ts to explicitly set environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
-export default defineConfig({
-  plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
+    define: {
+      'process.env.SANITY_PROJECT_ID': JSON.stringify(env.SANITY_PROJECT_ID || process.env.SANITY_PROJECT_ID),
+      'process.env.SANITY_DATASET': JSON.stringify(env.SANITY_DATASET || process.env.SANITY_DATASET),
+    },
+  };
 });
