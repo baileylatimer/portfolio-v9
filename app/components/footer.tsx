d89e41adb4de09f985ea3a3d2ee9db66900340dc
Bailey Latimer <baileyl@beautyindustrygroup.com>
2024-10-12 17:21:12 -0700

    updated footer (needs work)

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -8,29 +8,29 @@ import CustomButton from './custom-button';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="relative bg-cover bg-center py-8" style={{backgroundImage: "url('/images/footer-bg.png')"}}>
-      <div className="container mx-auto px-4">
-        <div className="desktop hidden md:block">
-          <div className="footer-text flex justify-start items-center">
-            Hollywood <SvgCoordinates /> Los Angeles <SvgCaFlag />
+    <footer className="relative bg-cover bg-bottom pt-48 py-8 overflow-hidden" style={{backgroundImage: "url('/images/footer-bg.png')"}}>
+      <div className="container mx-auto px-4 footer-content pb-24">
+        <div className="desktop hidden md:block font-secondary ">
+          <div className="flex justify-start items-center whitespace-nowrap footer-row">
+           <SvgStar />Hollywood <span className='mx-4'><SvgCoordinates /></span> Los Angeles <span className='ml-4'><SvgCaFlag width={200} height={180}/></span>
           </div>
-          <div className="footer-text flex justify-start items-center">
-            Creative Studio <SvgStar /> Bailey <SvgPeaceSign /> Latimer <SvgEmailArrow /> bailey@latimer.me
+          <div className="flex justify-start items-center whitespace-nowrap footer-row">
+            Creative Studio <span className='font-default mx-4'>by</span> Bailey Latimer <span className='block mx-4'><SvgPeaceSign /></span>  bailey <SvgEmailArrow />latimer.me
           </div>
         </div>
 
-        <div className="mobile md:hidden">
-          <div className="footer-text flex justify-start items-center">
-            Hollywood <SvgCoordinates />
+        <div className="mobile md:hidden font-secondary">
+          <div className="flex justify-start items-center">
+          <SvgStar /> Hollywood <SvgCoordinates />
           </div>
-          <div className="footer-text flex justify-start items-center">
+          <div className="flex justify-start items-center">
             Los Angeles <SvgCaFlag />
           </div>
-          <div className="footer-text flex justify-between items-center mt-2 mb-4">
-            Creative Studio <SvgStar />
+          <div className="flex justify-between items-center mt-2 mb-4 whitespace-nowrap">
+            Creative Studio <span className='font-default'>by</span>
           </div>
-          <div className="footer-text flex justify-start items-center">
-            Bailey <SvgPeaceSign /> Latimer <SvgEmailArrow /> bailey@latimer.me
+          <div className="flex justify-start items-center">
+            Bailey Latimer <SvgPeaceSign /> bailey <SvgEmailArrow />latimer.me
           </div>
         </div>
 
@@ -38,10 +38,11 @@ const Footer: React.FC = () => {
           <CustomButton>LET&apos;S TALK</CustomButton>
         </div>
 
-        <div className="mt-8 text-center">
-          <p className="font-accent footer-text">©2024 LATIMER</p>
-        </div>
+
+
+
       </div>
+      <p className="font-accent footer-text">LATIMER</p>
     </footer>
   );
 };
