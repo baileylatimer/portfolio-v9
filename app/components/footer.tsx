eb43a6385c6d3b7c60c00a9c29b2b630039e0d0f
Bailey Latimer <baileyl@beautyindustrygroup.com>
2024-10-12 17:31:54 -0700

    updated footer (needs work)

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -7,42 +7,48 @@ import SvgEmailArrow from './svg-email-arrow';
 import CustomButton from './custom-button';
 
 const Footer: React.FC = () => {
+  const currentYear = new Date().getFullYear();
   return (
-    <footer className="relative bg-cover bg-bottom pt-48 py-8 overflow-hidden" style={{backgroundImage: "url('/images/footer-bg.png')"}}>
-      <div className="container mx-auto px-4 footer-content pb-24">
-        <div className="desktop hidden md:block font-secondary ">
-          <div className="flex justify-start items-center whitespace-nowrap footer-row">
-           <SvgStar />Hollywood <span className='mx-4'><SvgCoordinates /></span> Los Angeles <span className='ml-4'><SvgCaFlag width={200} height={180}/></span>
-          </div>
-          <div className="flex justify-start items-center whitespace-nowrap footer-row">
-            Creative Studio <span className='font-default mx-4'>by</span> Bailey Latimer <span className='block mx-4'><SvgPeaceSign /></span>  bailey <SvgEmailArrow />latimer.me
-          </div>
-        </div>
-
-        <div className="mobile md:hidden font-secondary">
-          <div className="flex justify-start items-center">
-          <SvgStar /> Hollywood <SvgCoordinates />
-          </div>
-          <div className="flex justify-start items-center">
-            Los Angeles <SvgCaFlag />
-          </div>
-          <div className="flex justify-between items-center mt-2 mb-4 whitespace-nowrap">
-            Creative Studio <span className='font-default'>by</span>
-          </div>
-          <div className="flex justify-start items-center">
-            Bailey Latimer <SvgPeaceSign /> bailey <SvgEmailArrow />latimer.me
-          </div>
-        </div>
-
-        <div className="mt-8 text-center">
+    <footer className="relative bg-cover bg-bottom pt-36 " style={{backgroundImage: "url('/images/footer-bg.png')"}}>
+      <div className='overflow-hidden relative'>
+        <div className="container mx-auto px-4 footer-content pb-36">
+          <div className='flex justify-between w-full pb-12'>
+          <h4 className='eyebrow mix-blend-difference'>Take your site to the next level</h4>
           <CustomButton>LET&apos;S TALK</CustomButton>
+          </div>
+  
+          <div className="desktop hidden md:block font-secondary pb-8">
+            <div className="flex justify-start items-center whitespace-nowrap footer-row  mix-blend-difference">
+            <SvgStar />Hollywood <span className='mx-4'><SvgCoordinates /></span> Los Angeles <span className='ml-4'><SvgCaFlag width={200} height={180}/></span>
+            </div>
+            <div className="flex justify-start items-center whitespace-nowrap footer-row  mix-blend-difference">
+              Creative Studio <span className='font-default mx-4'>by</span> Bailey Latimer <span className='block mx-4'><SvgPeaceSign /></span>  bailey <SvgEmailArrow />latimer.me
+            </div>
+          </div>
+
+          <div className="mobile md:hidden font-secondary">
+            <div className="flex justify-start items-center  mix-blend-difference">
+            <SvgStar /> Hollywood <SvgCoordinates />
+            </div>
+            <div className="flex justify-start items-center  mix-blend-difference">
+              Los Angeles <SvgCaFlag />
+            </div>
+            <div className="flex justify-between items-center mt-2 mb-4 whitespace-nowrap  mix-blend-difference">
+              Creative Studio <span className='font-default'>by</span>
+            </div>
+            <div className="flex justify-start items-center  mix-blend-difference">
+              Bailey Latimer <SvgPeaceSign /> bailey <SvgEmailArrow />latimer.me
+            </div>
+          </div>
+
+          <div className='flex justify-between w-full mix-blend-difference'> </div>
+          <span>©{currentYear}</span>
+          <div className='flex gap-4'>
+            <a href="https://www.instagram.com/latimer2k/">Instagram,</a>
+          </div>
         </div>
-
-
-
-
+        <p className="font-accent footer-text">LATIMER</p>
       </div>
-      <p className="font-accent footer-text">LATIMER</p>
     </footer>
   );
 };
