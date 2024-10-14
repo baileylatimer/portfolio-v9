fdc63900e8ccab391ad12b095cedd5dbbe0fcd1a
Bailey Latimer <baileyl@beautyindustrygroup.com>
2024-10-13 23:14:02 -0700

    added tape to about page

--- app/components/image-with-text.tsx
+++ app/components/image-with-text.tsx
@@ -32,7 +32,15 @@ const ImageWithText: React.FC<ImageWithTextProps> = ({ title, content, image, im
             </div>
           </div>
           <div className="md:w-1/2 relative">
-            <img src={image.asset.url} alt={title} className="w-full h-auto" />
+            <div className="relative">
+              <img src={image.asset.url} alt={title} className="w-full h-auto" />
+              <div className="tape absolute  left-0 transform -rotate-12 -translate-x-1/6 -translate-y-1/6">
+                <img src="/images/tape.png" alt="Tape" className="w-64 h-auto" />
+              </div>
+              <div className="tape absolute  right-0 transform rotate-12 translate-x-1/6 -translate-y-1/6">
+                <img src="/images/tape.png" alt="Tape" className="w-48 h-auto" />
+              </div>
+            </div>
             <div className="absolute bottom-4 right-4 color-bg uppercase p-2">
               {imageExcerpt}
             </div>
