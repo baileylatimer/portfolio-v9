d472ab9b6c980fdf219870fed7ffebe07a8c1d09
Bailey Latimer <baileyl@beautyindustrygroup.com>
2024-10-13 23:17:46 -0700

    updated tape

--- app/components/image-with-text.tsx
+++ app/components/image-with-text.tsx
@@ -35,10 +35,10 @@ const ImageWithText: React.FC<ImageWithTextProps> = ({ title, content, image, im
             <div className="relative">
               <img src={image.asset.url} alt={title} className="w-full h-auto" />
               <div className="tape absolute  left-0 transform -rotate-12 -translate-x-1/6 -translate-y-1/6">
-                <img src="/images/tape.png" alt="Tape" className="w-64 h-auto" />
+                <img src="/images/tape.png" alt="Tape" className="w-64 lg:w-96 h-auto" />
               </div>
-              <div className="tape absolute  right-0 transform rotate-12 translate-x-1/6 -translate-y-1/6">
-                <img src="/images/tape.png" alt="Tape" className="w-48 h-auto" />
+              <div className="tape absolute  right-0 transform rotate-6 translate-x-1/6 -translate-y-1/6">
+                <img src="/images/tape.png" alt="Tape" className=" w-48 lg:w-96 h-auto" />
               </div>
             </div>
             <div className="absolute bottom-4 right-4 color-bg uppercase p-2">
