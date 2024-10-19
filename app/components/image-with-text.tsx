6f43365dc6bb0bdd2f116ef07718b79eba6dbed4
Bailey Latimer <baileyl@beautyindustrygroup.com>
2024-10-18 19:58:33 -0700

    added pixelate to about page

--- app/components/image-with-text.tsx
+++ app/components/image-with-text.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { PortableText } from '@portabletext/react';
+import PixelizeImage from './PixelizeImage';
+import { PortableTextBlock } from '@portabletext/types';
 
 interface ImageWithTextProps {
   title: string;
-  content: any[];
+  content: PortableTextBlock[];
   image: {
     asset: {
       url: string;
@@ -33,12 +35,16 @@ const ImageWithText: React.FC<ImageWithTextProps> = ({ title, content, image, im
           </div>
           <div className="md:w-1/2 relative light-section">
             <div className="relative light-section">
-              <img src={image.asset.url} alt={title} className="w-full h-auto" />
-              <div className="tape absolute  left-0 transform -rotate-12 -translate-x-1/6 -translate-y-1/6">
+              <PixelizeImage
+                src={image.asset.url}
+                alt={title}
+                className="w-full h-auto"
+              />
+              <div className="tape absolute left-0 transform -rotate-12 -translate-x-1/6 -translate-y-1/6">
                 <img src="/images/tape.png" alt="Tape" className="w-64 lg:w-96 h-auto" />
               </div>
-              <div className="tape absolute  right-0 transform rotate-6 translate-x-1/6 -translate-y-1/6">
-                <img src="/images/tape.png" alt="Tape" className=" w-48 lg:w-96 h-auto" />
+              <div className="tape absolute right-0 transform rotate-6 translate-x-1/6 -translate-y-1/6">
+                <img src="/images/tape.png" alt="Tape" className="w-48 lg:w-96 h-auto" />
               </div>
             </div>
             <div className="absolute bottom-4 right-4 color-bg uppercase p-2">
